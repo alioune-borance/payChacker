c9311e2ea6dcb25bc95db40513e35051f74bd403
alioune-borance <borancembaye98@gmail.com>
2022-02-26 18:00:21 +0000

    second com

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import session,render_template, flash ,request, Flask, flash, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import backref
@@ -20,7 +21,7 @@ app.secret_key = 'BAD_SECRET_KEY'
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/lambdabd2'
 
-#db= SQLAlchemy(app)
+db= SQLAlchemy(app)
 
 ALLOWED_EXTENSIONS = {'jar'}
 #app.config['UPLOAD_FOLDER'] = 'C://Users//Administrator//Desktop//stage'
@@ -29,8 +30,48 @@ ALLOWED_EXTENSIONS = {'jar'}
 
 #DEFINITION DU MODELE(BASE DE DONNEES)
 
+class Ged_employee(db.Model):
+    """
+    Create a table
+    """
 
-#db.create_all()
+    # Ensures table will be named in plural and not in singular
+    # as is the name of the model
+    __tablename__ = 'ged_employee'
+
+    employee_id = db.Column(db.Integer, primary_key=True)
+    employee_email = db.Column(db.String(500),unique=True)
+    email_passwd = db.Column(db.String(500),unique=True)
+    employee_fullname = db.Column(db.String(500))
+    employee_role = db.Column(db.String(500))
+
+    #scenarios = db.relationship('Scenario',backref='integrateur',lazy=True)
+
+    def __repr__(self):
+        return '<ged_employee: {}>'.format(self.id)
+
+class Ged_file(db.Model):
+    """
+    Create a table
+    """
+
+    # Ensures table will be named in plural and not in singular
+    # as is the name of the model
+    __tablename__ = 'ged_file'
+
+    file_id = db.Column(db.Integer, primary_key=True)
+    file_name = db.Column(db.String(500),unique=True)
+    file_type = db.Column(db.String(500),unique=True)
+    file_src = db.Column(db.String(500))
+    file_accountId = db.Column(db.String(500))
+
+    #scenarios = db.relationship('Scenario',backref='integrateur',lazy=True)
+
+    def __repr__(self):
+        return '<ged_employee: {}>'.format(self.id)
+
+
+db.create_all()
 
 
 
@@ -42,12 +83,23 @@ ALLOWED_EXTENSIONS = {'jar'}
 #---------------
 @app.route("/",methods=['GET','POST'])
 def index():
-
     return render_template("index.html")
 
+@app.route("/connexion",methods=['GET','POST'])
+def connexion():
+    p = request.form.get('email')
+    e = Ged_employee.query.filter_by(employee_email=p)
+    if e != None:
+        if (request.form.get('mdp') == e.email_passwd):
+            return p
+    return "p"
 
+@app.route("/recherche",methods=['GET','POST'])
+def recherche():
+    r = request.form.get('rech')
+    files = Ged_file.file_name.like("%"+r+"%")
 
-
+    return render_template("files.html",fs=files)
 
 if __name__ == '__main__':
     app.run()
